7a40c736af8ac888b150fd3d2197ccaab0c54581
PadeepaChamika <padeepachamika12@gmail.com>
2022-08-16 00:00:32 +0530

    update

--- src/LoginInitializer.java
+++ src/LoginInitializer.java
@@ -1,18 +1,20 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class ChatRoomInitializer extends Application {
+public class LoginInitializer extends Application {
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("ChatRoomForm.fxml"))));
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("views/LoginForm.fxml"))));
         primaryStage.show();
     }
 }
